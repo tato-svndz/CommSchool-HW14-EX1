21e398745b353ccbaca1eacdfb9051ee660f4810
Tato Svndz <tato.svndz@gmail.com>
2023-08-10 21:21:41 +0400

    made my life easier

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 function isPrime(n) {
-    if (n <= 1) {
-        return false;  
-    } 
-    if (n <= 3) {
+    if (n === 1 || n === 0) {
+        return false;
+    } else {
+        for (let i=2; i < n; i++) {
+            if(number % i === 0) {
+                return false;
+            }
+        }
         return true;
     }
-    if (n % 2 === 0 || n % 3 === 0) {
-        return false;
-    }    
-    for (let i = 5; i * i <= n; i += 6) {
-        if (n % i === 0 || n % (i + 2) === 0) {
-          return false;
-        }
-      }
-        return true;
 }   
 
 function showPrimes(number) {
     let primeNumbers = '';
-    if (number == 0 || number == 1) { 
-        alert('Neither a Prime, Nor a Composite');
-        return;
-    }
-    for (let i = 1; i <= number; i++) {
+    for (let i = 2; i <= number; i++) {
         if (isPrime(i)) {
             primeNumbers += ` ${i}`;
         }
     }
-    alert(primeNumbers);  
+    return primeNumbers;  
     }
 
-showPrimes(prompt('Enter a Number:'));
+let num = prompt('Enter a Number:')
+
+alert(showPrimes(num));
